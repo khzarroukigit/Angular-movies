d3099544e845489dcd3f830cd8e839c217af6d0a
khzarroukigit <khzarroukigit@gmail.com>
2021-10-15 11:54:40 +0100

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IndexGenresComponent } from './genres/index-genres/index-genres.component';
 import { HomeComponent } from './home/home.component';
-import { RatingComponent } from './utilities/rating/rating.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
