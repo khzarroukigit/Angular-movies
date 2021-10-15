7414be3c624d1f03e161af6f94f37e6fd8952c97
khzarroukigit <khzarroukigit@gmail.com>
2021-10-15 17:26:28 +0100

    routing work !!!

---
+++ src/app/movies/create-movie/create-movie.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-create-movie',
+  templateUrl: './create-movie.component.html',
+  styleUrls: ['./create-movie.component.css']
+})
+export class CreateMovieComponent implements OnInit {
+
+  constructor(private router:Router) { }
+
+  ngOnInit(): void {
+  }
+  save(){ 
+    this.router.navigate(['']);
+  }
+
+
+}
